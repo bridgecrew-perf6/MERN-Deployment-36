a20d4c819ae47d9916bf18393c26034538518824
joelemm17 <joelemay06@gmail.com>
2022-01-28 23:26:19 -0500

    Second commit

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ const app = express();
 app.use(cors());
 app.use(express.json(), express.urlencoded({ extended: true }));
 
-require("./server/config/mongoose.config");
-require("./server/routes/pet.routes")(app);
+require("./config/mongoose.config");
+require("./routes/pet.routes")(app);
 // AllMyPetRoutes(app);
 
 app.listen(8000, () => console.log("The server is all fired up on port 8000"));
